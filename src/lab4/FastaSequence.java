16e3bc6927574011d1bd44494ce12de925d222bc
Tyler <Tyler@10.38.41.41>
2017-09-27 09:36:36 -0400

    this one is for lab 5

--- src/lab4/FastaSequence.java
+++ src/lab4/FastaSequence.java
@@ -3,10 +3,18 @@
 package lab4;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class FastaSequence {
 	
@@ -24,16 +32,20 @@ public class FastaSequence {
 	public static void main(String[] args) throws Exception {
 	//create new list to contain the file 
 		List<FastaSequence> fastaList = 
-				FastaSequence.readFastaFile("C:\\Users\\Tyler\\Desktop\\sequence.txt");
+				FastaSequence.readFastaFile("C:\\Users\\Tyler\\Desktop\\sequence2.txt");
 		
 		for( FastaSequence fs : fastaList)
 		{
 		System.out.println(fs.getHeader());
 		System.out.println(fs.getSequence());
-		System.out.println(fs.getGCRatio());
-		System.out.println("\n");
+		//System.out.println(fs.getGCRatio());
+		//System.out.println("\n");
 		}
 
+		File input = new File("C:\\Users\\Tyler\\Desktop\\sequence2.txt");
+		File output = new File("C:\\Users\\Tyler\\Desktop\\output.txt");
+		
+		writeUnique(input,output);
 	}
 	//factory method to parse file being fed into it
 	public static List<FastaSequence> readFastaFile(String filepath) throws Exception{
@@ -41,15 +53,15 @@ public class FastaSequence {
 		List<FastaSequence> myList = new ArrayList<FastaSequence>();
 		//create new buffered reader to read in the file
 		BufferedReader reader = 
-				new BufferedReader(new FileReader(new File("C:\\Users\\Tyler\\Desktop\\sequence.txt")));
+				new BufferedReader(new FileReader(new File("C:\\Users\\Tyler\\Desktop\\sequence2.txt")));
 		//create blank variables to hold values for header and sequence and keep track of where we are in the file
-		String header = "";
+		String header = null;
 		String sequence = "";
 		
 		//read through the file
 		for(String s = reader.readLine(); s!=null; s=reader.readLine()) {
 			//if header is blank, its the first time through the file and get header
-			if(header == "") {
+			if(header == null) {
 				if(s.startsWith(">")) {
 					header = s.substring(1);
 				}	
@@ -76,6 +88,55 @@ public class FastaSequence {
 		reader.close();
 		return myList;
 	}
+	
+	public static void writeUnique(File inFile, File outFile ) throws Exception {
+		
+		//use readfastafile method to read in file and put into list, and make a file writer
+		List<FastaSequence> myList = readFastaFile(inFile.getAbsolutePath());
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+		
+		//create the hashmap to store the sequences and the count 
+		HashMap<String,Integer> myHashMap = new HashMap<String, Integer>();
+		
+		String seq= "";
+		int count = 0;
+		
+		//scan through the list and put in the sequences as well as increment the count for ones already there
+		for(FastaSequence sequence : myList) {
+			seq = sequence.getSequence();
+			if(!myHashMap.containsKey(seq)) {
+				myHashMap.put(seq, 1);
+			}
+			else {
+				myHashMap.put(seq, myHashMap.get(seq)+1);
+			}
+			
+		}
+		
+		//tester loop to print to the console 
+		for(String key : myHashMap.keySet()) {
+			System.out.println(key + " " + myHashMap.get(key));
+	
+		}
+		
+		//make an array list to store the values and use this to sort them
+		List <Entry<String,Integer>>myArrayList = new ArrayList(myHashMap.entrySet());
+		
+		//sort the array list
+		myArrayList.sort((o1,o2)->((Comparable<Integer>)((Map.Entry)(o1)).getValue()).compareTo((Integer)((Map.Entry)(o2)).getValue()));
+				
+		//write the output to a file 
+			for(Entry<String,Integer> data : myArrayList) {
+				writer.write(">" + data.getValue());
+				writer.newLine();
+				writer.write(data.getKey());
+				writer.newLine();
+			}
+			
+			writer.close();
+			
+		}
+
 	
 
 		public String getHeader() {
